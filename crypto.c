2ca7308214881541dacfde5fe1f07d229b5b647e
Moaan <moaana@live.com>
2014-09-08 19:59:16 -0700

    Second

--- crypto.c
+++ crypto.c
@@ -1,10 +1,3 @@
-/******************************************************************************\												                           
-|	Program : crypto.c						                           
-|	Problem : An encryption algorithm.
-|	Method  : user's gives key. (xor operation w/ pseudo-random bytes from key)	                               
-|	Author  : Moaan Ahmed							                         
-|	Date    : 09/06/2014		                   										                               
-\******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #define bufferSize 600
@@ -16,7 +9,9 @@ unsigned char readBuffer[bufferSize];
 int bytesCopied, i, j;
 unsigned char c, r;
 unsigned int seed;
+char *mode; //encryption or decryption mode. Needed for swapping only.
 
+//the xor function does the confusion
 unsigned char xor(unsigned char a, unsigned char r)
 {
 	unsigned char c = 0;
@@ -30,10 +25,25 @@ unsigned char xor(unsigned char a, unsigned char r)
 	return c;
 }
 
+//the swap function does the diffusion
+void swap(int copiedBytes) 
+{
+	int midpoint = copiedBytes/2;
+	char tempC;
+	int i, j;
+	//swap first and last chars of buffer until you reach the middle.
+	for(i = 0, j = copiedBytes -1; i < midpoint; i++,j--)
+	{
+		tempC = readBuffer[i];
+		readBuffer[i] = readBuffer[j];
+		readBuffer[j] = tempC;
+	}	
+}
+
 int main(int argc, char**argv)
 {
-	if(argc < 3) {
-		printf("Incorrect Usage. Include parameters: [input file] [output file]\n");
+	if(argc < 4) {
+		printf("Incorrect Usage. \nInclude params: [\"-e\" | \"-d\"] [input file] [output file]\n");
 		return -1;	
 	}
 
@@ -41,12 +51,16 @@ int main(int argc, char**argv)
 	fgets(key, 90, stdin);
 	
 	//sets the seed based on the key.
-	for(i=0; key[i]; i++) seed = (seed + 2)*key[i];
+	int seedAddition = key[0]; //Maybe no affect but it could make it encryption weaker or stronger. ....analyze.
+	for(i=0; key[i]; i++) seed = (seed + seedAddition)*key[i];
 	srand(seed);
 	
+	//determine if we're trying to encrypt or decrypt
+	mode = argv[1];
+	
 	//open both files
-	inFileName = argv[1];
-	outFileName = argv[2];	
+	inFileName = argv[2];
+	outFileName = argv[3];	
 	
 	//open input file
 	inFile = fopen(inFileName, "r");
@@ -64,30 +78,29 @@ int main(int argc, char**argv)
 		return 1;
 	}
 
-	//read from input file to char array. encrypt char array and write it to output file
+	/*read from input file to char array. encrypt char array and write it to output file.
+	 *The array's contents will be swapped such that the last element swapped with element 0,
+	 *second-to-last element is swapped with element 1, etc.
+	 *If encrypting, swapping will be done before substitution, If decrypting, it will be done after.
+	 */
 	while(1) 
 	{
 		bytesCopied = fread(readBuffer, sizeof(char), bufferSize, inFile);
+		
+		if(mode == "-e") swap(bytesCopied);
+		
 		for(i = 0;i < bytesCopied; i++) 
 		{
 			c = readBuffer[i];
 			r = rand();
 			readBuffer[i] = xor(c, r);
 		}
+		
+		if(mode == "-d") swap(bytesCopied);
+		
 		fwrite(readBuffer, sizeof(char), bytesCopied, outFile);
 		if(bytesCopied < bufferSize) break;
 	}
-	
-	
-	//read one char. encrypt/decrypt it. write it.
-	/*
-	c = fgetc(inFile);
-	while(c != EOF)
-	{
-		r = rand();
-		fputc(xor(c, r), outFile);
-		c = fgetc(inFile);
-	}*/
 	
 	fclose(outFile);
 	fclose(inFile);
